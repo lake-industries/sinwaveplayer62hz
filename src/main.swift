b1067e6321b98e1d395464bbc90334c19df61dff
lake-industries <seanarnold2@yahoo.com>
2026-01-13 08:03:03 -0700

    Add blank main.swift starter file

---
+++ src/main.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, iOS App Publisher!")
+                .font(.title)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
